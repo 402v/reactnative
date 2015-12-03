0265c5beab9384fcedf555862d682b06e95a5573
kimimaro <kimirius@gmail.com>
2015-12-03 14:30:23 +0800

    add segmented control

--- firststep/index.ios.js
+++ firststep/index.ios.js
@@ -10,44 +10,127 @@ var {
   StyleSheet,
   Text,
   View,
+  Image,
+  Navigator,
+  ListView,
+  TouchableOpacity,
+  TouchableHighlight,
+  SegmentedControlIOS,
 } = React;
 
-var firststep = React.createClass({
+// Scenes
+var SCREEN_WIDTH = require('Dimensions').get('window').width;
+var BaseConfig = Navigator.SceneConfigs.FloatFromRight;
+
+var CustomLeftToRightGesture = Object.assign({}, BaseConfig.gestures.pop, {
+  snapVelocity: 8,
+  edgeHitWidth: SCREEN_WIDTH,
+});
+
+var CustomSceneConfig = Object.assign({}, BaseConfig, {
+  springTension: 100,
+  springFriction: 1,
+
+  gestures: {
+    pop: CustomLeftToRightGesture,
+  }
+});
+
+// detail page
+var DetailPage = React.createClass({
+  render: function() {
+    <View style={styles.container} />
+  }
+});
+
+// list page
+var ListPage = React.createClass({
+  getInitialState() {
+    return {
+      titles: ['最新', '分类'],
+      selectedIndex: 0,
+      welcomeText: '最新',
+    };
+  },
+
+  render() {
+    return (
+      <View style={[styles.container]}>
+        <Text style={styles.welcome}>{this.state.welcomeText}</Text>
+
+        // pre selected & color & event segmented control
+        <SegmentedControlIOS 
+          style={styles.segment} 
+          values={this.state.titles}
+          selectedIndex={this.state.selectedIndex}
+          onChange={this._onChange}
+          onValueChange={this._onValueChange} />
+      </View>
+    );
+  },
+
+  _onChange(event) {
+    this.setState({
+      selectedIndex: event.nativeEvent.selectedSegmentIndex,
+    });
+  },
+
+  _onValueChange(value) {
+    this.setState({
+      welcomeText: value
+    });
+  }
+});
+
+// my App
+var Firststep = React.createClass({
+
+  _renderScene(route, navigator) {
+    if (route.id === 1) {
+      return <ListPage navigator={navigator} />
+    } else if (route.id === 2) {
+      return <DetailPage navigator={navigator} />
+    };
+  },
+
+  _configureScene(route) {
+    return CustomSceneConfig;
+  },
+
   render: function() {
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
-        </Text>
-      </View>
+      <Navigator
+        initialRoute={{id: 1, }}
+        renderScene={this._renderScene}
+        configureScene={this._configureScene} />
     );
   }
 });
 
 var styles = StyleSheet.create({
-  container: {
+  container: {  // ListView, ListCell
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
+    // justifyContent: 'center',
+    // alignItems: 'center',
+    paddingLeft: 10,
+    paddingRight: 10,
+    marginTop: 20,
+    // marginBottom: 10,
+    backgroundColor: 'purple',//'#F5FCFF',
   },
-  welcome: {
+  segment: {
+    tintColor: "#ff0000",
+  },
+  welcome: {  // text
     fontSize: 20,
     textAlign: 'center',
     margin: 10,
   },
-  instructions: {
+  instructions: { // button
     textAlign: 'center',
     color: '#333333',
     marginBottom: 5,
   },
 });
 
-AppRegistry.registerComponent('firststep', () => firststep);
+AppRegistry.registerComponent('firststep', () => Firststep);
